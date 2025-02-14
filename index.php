1ec9da7375c65e057ed40ab00cc062067f136ae6
raicassia <rainamodoyuta127@gmail.com>
2025-02-14 11:40:57 -0300

    14/02

--- index.php
+++ index.php
@@ -7,8 +7,56 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</head>
+    </head>
 <body>
-    <h1>Projeto SW2</h1>
+    <div class="container-fluid bg-primary text-white text-center p-5">
+         <h1>Projeto SW2 </h1>
+         <p>
+            Elaborado por rai rai
+    </div>
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-sm-4">
+                <h1 class="bg-primary text-warning">Administrador</h1>
+                <p class="text-primary">
+                    Uitilize esta opção para realizar o cadastro de novos administradores, ou edita-los.
+                </p>
+            </div>
+
+
+            <div class="col-sm-4">
+                <h1 class="bg-danger">Categorias</h1>
+                <p class="text-danger">
+                    Utilize esta opção para cadastrar as categorias dos produtos que serão vendidos on-line.
+                </p>
+            </div>
+
+
+            <div class="col-sm-4">
+                <h1 class="bg-success">Fornecedor</h1>
+                <p class="text-success">
+                    Utilize esta opção para cadastrar os fornecedores dos produtos que serão vendidos on-line.
+                </p>
+            </div>
+        </div>
+
+            <div class="row">
+                <div class="col-sm-6">
+                    <h1>Produtos</h1>
+            </div>
+
+            
+                <div class="col-sm-6">
+                    <h1>Vendas</h1>
+            </div>
+
+
+
+
+    </div>
+    </div>
+    
 </body>
 </html>
