1e389aea8a7c5fff4bb68037ca17c91b9ac5878b
raicassia <rainamodoyuta127@gmail.com>
2025-03-25 11:27:33 -0300

    25/03/2025

--- projetosw.sql
+++ projetosw.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 18-Mar-2025 às 13:25
+-- Tempo de geração: 25-Mar-2025 às 14:25
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -35,6 +35,32 @@ CREATE TABLE IF NOT EXISTS `administrador` (
   `nome` varchar(200) NOT NULL,
   `login` varchar(200) NOT NULL,
   `senha` varchar(200) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+DROP TABLE IF EXISTS `categoria`;
+CREATE TABLE IF NOT EXISTS `categoria` (
+  `codigo` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `fornecedor`
+--
+
+DROP TABLE IF EXISTS `fornecedor`;
+CREATE TABLE IF NOT EXISTS `fornecedor` (
+  `codigo` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
   PRIMARY KEY (`codigo`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 COMMIT;
